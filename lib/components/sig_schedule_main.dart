2355d79c6cc3c52945e75a279cf8de98cbda7786
sig03 <sig03@naver.com>
2023-06-01 08:17:47 +0900

    스크롤 부분 확인필요

--- lib/components/sig_schedule_main.dart
+++ lib/components/sig_schedule_main.dart
@@ -7,23 +7,106 @@ class SigScheduleMain extends StatelessWidget {
       appBar: AppBar(
         title: Text('SigSchedule'),
       ),
-      body: Center(
-        child: Text('Main'),
+      body: Scrollbar(
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                child: ListTile(
+                  title: Text('약 먹기2'),
+                  subtitle: Text('2023.06.01. 13:00'),
+                  trailing: Icon(Icons.delete),
+                ),
+              ),
+            ),
+          ],
+      ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: 'favorite'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.delete),
-              label: 'trash',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            label: 'add',
-          ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'favorite'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.delete),
+            label: 'trash',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_circle),
+          label: 'add',
+        ),
         ],
       ),
     );
